a5fe882558f912544c0e4b98c33300b323390d02
James Prince <james.prince1@gmail.com>
2022-10-30 14:27:32 +1100

    Added some deletion methods. Updated the database action to null the role fields if department and all associated roles get deleted

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -18,17 +18,19 @@ CREATE TABLE role (
     PRIMARY KEY (id),
     FOREIGN KEY (department_id)
     REFERENCES department(id)
+    ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
+    role_id INT,
     manager_id INT,
     PRIMARY KEY (id),
     FOREIGN KEY (role_id)
-    REFERENCES role(id),
+    REFERENCES role(id)
+    ON DELETE SET NULL,
     FOREIGN KEY (manager_id)
     REFERENCES employee(id)
 );
